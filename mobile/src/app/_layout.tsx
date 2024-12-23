52ac10197485fe41a5641b974bd1265506dfa450
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.com.br>
2024-12-23 13:21:28 -0300

    ponto seguro

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,9 +1,34 @@
 import { Slot } from "expo-router";
+import { SafeAreaView } from "react-native";
 
 import "../styles/global.css"
 
+import {
+    useFonts,
+    Inter_400Regular, 
+    Inter_500Medium, 
+    Inter_600SemiBold, 
+    Inter_700Bold} from "@expo-google-fonts/inter"
+    
+import { Loading } from "@/components/loading";
+
 export default function Component(){
+
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular, 
+        Inter_500Medium, 
+        Inter_600SemiBold, 
+        Inter_700Bold
+    })
+
+    if(!fontsLoaded){
+        return <Loading />
+    }
+
+
     return (
-        <Slot/>
+        <SafeAreaView className="bg-slate-900 flex-1 ">
+            <Slot/>
+        </SafeAreaView>
     )
 }
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Home(){
     return (
-        <View className="bg-black flex-1 ">
-            <Text className="text-white">Hello React Native!</Text>
+        <View className="flex-1 pt-8">
+            <Text className="text-white text-2xl">Hello React Native!</Text>
         </View>
     )
 }
