1ed5948cc920119fa08429c073ca67bdfa7452e2
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.com.br>
2024-12-23 14:43:01 -0300

    ponto seguro flatlist category

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,10 +1,25 @@
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+
+import {CATEGORIES} from "@/utils/data/products"
 
 export default function Home(){
     return (
         <View className="flex-1 pt-8">
             <Header title="Faça o seu pedido" cardQuantityItems={1}/>
+
+            <FlatList  
+            className="max-h-10 mt-5"
+            data={CATEGORIES}
+            keyExtractor={(item) => item}
+            horizontal
+            showsHorizontalScrollIndicator = {false}
+            contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+            renderItem={({item}) => (
+                <CategoryButton title={item}/>
+            )}
+            />
         </View>
     )
 }
