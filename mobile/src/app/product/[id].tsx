3b3e1f9c01bd44a0d806d3d3bb6b987ef0eca556
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.com.br>
2024-12-26 16:49:20 -0300

    ponto seguro

--- mobile/src/app/product/[id].tsx
+++ mobile/src/app/product/[id].tsx
@@ -5,7 +5,7 @@ import { formatCurrency } from "@/utils/functions/format-currency";
 import { Button } from "@/components/button";
 import { Feather } from "@expo/vector-icons";
 import { LinkButton } from "@/components/link-button";
-
+import {Redirect} from "expo-router"
 import { useCartStore } from "@/stores/cart-store";
 
 export default function Product(){
@@ -13,18 +13,26 @@ export default function Product(){
     const navigation = useNavigation()
     const {id} = useLocalSearchParams()
 
-    const product = PRODUCTS.filter((item) => item.id === id)[0]
+    const product = PRODUCTS.find((item) => item.id === id)
     
     function handleAddToCart() {
-        cartStore.add(product)
-        navigation.goBack()
+        if(product){
+            cartStore.add(product)
+            navigation.goBack()
+        }
+    }
+
+    if(!product) {
+        return <Redirect href="/" />
     }
     
     return (
         <View className="flex-1">
-            <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>
+            <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>            
 
             <View className="p-5 mt-8 flex-1">
+                <Text className="text-white text-xl font-heading">{product.title}</Text>
+
                 <Text className="text-lime-400 text-2xl font-heading my-2">
                     {formatCurrency(product.price)}
                 </Text>
