5a3909fd3a631246251639a5627a542d145ebfd7
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.com.br>
2024-12-27 10:24:28 -0300

    ponto seguro

---
+++ mobile/src/app/cart.tsx
@@ -0,0 +1,66 @@
+import { ScrollView, View, Text } from "react-native";
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view"
+
+import { useCartStore } from "@/stores/cart-store";
+
+import { Header } from "@/components/header";
+import { Product } from "@/components/product";
+import { Input } from "@/components/input";
+
+
+import { formatCurrency } from "@/utils/functions/format-currency";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+
+export default function Cart(){
+    const cartStore = useCartStore()
+
+    const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0))
+    
+    return (
+        <View className="flex-1 pt-8">
+            <Header title="Seu carrinho" />
+
+            
+            <KeyboardAwareScrollView>
+                <ScrollView>
+                <View className="p-5 flex-1">
+                {
+                    cartStore.products.length > 0 ? (                    
+                    <View className="border-b border-slate-700">
+                    {
+                        cartStore.products.map((product) => (
+                            <Product key={product.id} data={product} />
+                        ))
+                    }
+                    </View>
+                    ) : (
+                        <Text className="font-body text-slate-400 text-center my-8">Seu carrinho está vazio.</Text>
+                    )
+                    
+                }
+                
+                </View>                
+
+                <View className="flex-row gap-2 items-center mt-5 mb-4">
+                    <Text className="text-white text-xl font-subtitle">Total:</Text>
+                    
+                    <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+                </View>
+
+                <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento." />
+
+            </ScrollView>            
+            </KeyboardAwareScrollView>
+
+            <View className="p-5 gap-5">
+                <Button>
+                    <Button.Text>Enviar Pedido</Button.Text>
+                    <Button.Icon>
+                        <Feather name="arrow-right-circle" size={20} />
+                    </Button.Icon>
+                </Button>
+            </View>
+        </View>
+    )
+}
